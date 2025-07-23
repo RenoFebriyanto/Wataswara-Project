747c64278a30a967a0011ad2457c477d2c87d790
Kafura <robbykafura@gmail.com>
2025-07-23 13:07:08 +0700

    senter

---
+++ Assets/sekrip/Player/flashlight.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class flashlight : MonoBehaviour
+{
+    public InputActionReference senter;
+    public Light spotlight;
+
+    void OnEnable()
+    {
+        senter.action.performed += FlashOn;
+        senter.action.Enable();
+    }
+
+    void OnDisable()
+    {
+        senter.action.performed -= FlashOn;
+        senter.action.Disable();
+    }
+
+    void FlashOn(InputAction.CallbackContext context)
+    {
+        if (context.interaction is UnityEngine.InputSystem.Interactions.TapInteraction)
+        {
+            ToggleFlashlight();
+        }
+    }
+
+    void ToggleFlashlight()
+    {
+        spotlight.enabled = !spotlight.enabled;
+    }
+}
--- Assets/sekrip/_Revealing/Reveal.cs
+++ Assets/sekrip/_Revealing/Reveal.cs
@@ -9,8 +9,19 @@ public class Reveal : MonoBehaviour
 	
 	void Update ()
     {
-        Mat.SetVector("_LightPosition",  SpotLight.transform.position);
-        Mat.SetVector("_LightDirection", -SpotLight.transform.forward );
-        Mat.SetFloat ("_LightAngle", SpotLight.spotAngle);
+    if (!Mat || !SpotLight) return;
+
+    if (SpotLight.enabled)
+    {
+        Mat.SetVector("_LightPosition", SpotLight.transform.position);
+        Mat.SetVector("_LightDirection", -SpotLight.transform.forward);
+        Mat.SetFloat("_LightAngle", SpotLight.spotAngle);
+    }
+    else
+    {
+        Mat.SetFloat("_LightAngle", 0);
+        Mat.SetVector("_LightDirection", Vector3.zero);
+        Mat.SetVector("_LightPosition", Vector3.one * 9999);
+    }
     }//Update() end
 }//class end
